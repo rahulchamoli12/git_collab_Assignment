a7aee19f5484619a176ac51a6330ca845bfe83ef
Rahul Chamoli <rahulchamoli518@gmail.com>
2022-10-21 00:23:09 +0530

    Prime and palindrome code

---
+++ palindrome.js
@@ -0,0 +1,20 @@
+let str = "naman"
+
+  let bag ="";
+  
+    for(let i =str.length-1;i>=0;i++){
+
+       bag += str[i];
+     
+     }
+       
+        if(bag == str){
+
+
+              console.log("its a Palindrome"){
+
+         }
+             else{
+         
+                  console.log("Not a Pallindrome")
+              }
---
+++ prime.js
@@ -0,0 +1,23 @@
+
+
+  let N =13;
+  let factor =0;
+   for(let i =1;i<=N;i++){
+   
+	if(N%i ==0){
+
+	factor++;
+
+	}
+
+    }
+    
+          if(factor ==3){
+
+            console.log("Prime no.")
+           
+           } else{
+
+                  console.log("Not Prime")
+               
+                }
